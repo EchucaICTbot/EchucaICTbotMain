fba969eab964140a983ad9844e407945aa67cd2b
Riley Brown <rileybrown3564@gmail.com>
2019-11-07 21:13:07 +1100

    make it better

---
+++ index.js
@@ -0,0 +1,104 @@
+// Extract the required classes from the discord.js module
+const { Client, Attachment } = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Client();
+
+var moment = require('moment');
+require('moment-recur');
+var schedule = require('node-schedule');
+var rInterval = moment( "2019-11-12" ).recur().every(2).weeks();
+
+
+
+
+
+
+
+function sendmessage() {
+    
+    var guild = client.guilds.get('518999425213792259');
+    if(guild && guild.channels.get('518999425646067717')){
+        guild.channels.get('518999425646067717').send("ICT Club will be at tomorrow lunch.");
+    }
+}
+
+function gettime() {
+var recurrence = moment("2019-11-11").recur().every(2).days();
+var ats = Date.now();
+
+        var adate_ob = new Date(ats);
+        var adate = adate_ob.getDate();
+        if (adate.toString().length == 1) {
+            adate = "0" + adate;
+        }
+        var amonth = adate_ob.getMonth() + 1;
+        if (amonth.toString().length == 1) {
+            amonth = "0" + amonth;
+        }
+        var ayear = adate_ob.getFullYear();
+
+recurrence.fromDate(ayear + "-" + amonth + "-" + adate);
+anextDates = recurrence.next(1, "L");
+var anextdateobject = new Date(anextDates);
+var anextdateDays = anextdateobject.getDate();
+var anextdateMonth = anextdateobject.getMonth() + 1;
+var anextdateYear = anextdateobject.getFullYear();
+    
+
+console.log(anextDates);
+console.log("00 12 " + anextdateDays + " " + anextdateMonth + " *");
+var j = schedule.scheduleJob("00 12 " + anextdateDays + " " + anextdateMonth + " *", function(){
+  sendmessage()
+  gettime()
+});
+
+}
+
+
+
+
+
+
+
+
+client.on('ready', () => {
+  console.log('I am ready!');
+  gettime()
+  
+});
+
+//client.on('message', message => {
+//    // If the message is '!time'
+//    if (message.content === '!time') {
+//        console.log("Got !time");
+//        var ts = Date.now();
+//
+//        var date_ob = new Date(ts);
+//        var date = date_ob.getDate();
+//        
+//        var month = date_ob.getMonth() + 1;
+//        
+//        var year = date_ob.getFullYear();
+//        rInterval.fromDate(year + "-" + month + "-" + date);
+//        nextDates = rInterval.next(1, "L");
+//        var nextdateobject = new Date(nextDates);
+//        var nextdateDays = nextdateobject.getDate();
+//        var nextdateMonth = nextdateobject.getMonth() + 1;
+//        var nextdateYear = nextdateobject.getFullYear();
+//        console.log("The next meeting will be on the :" + nextdateDays + "/" + nextdateMonth + "/" + nextdateYear);
+//        message.channel.send("The next meeting will be on the :" + nextdateDays + "/" + nextdateMonth + "/" + nextdateYear);
+//    }
+//});
+
+
+client.login('NjM4NjczOTE5ODEzMDkxMzY5.XblCjg.w1ckNAEGk3Tvnb2O6c5RRcWjE5A');
+
+
+
+
+
+
+        
+
+
---
+++ time.js
@@ -0,0 +1,58 @@
+// Extract the required classes from the discord.js module
+const { Client, Attachment } = require('discord.js');
+
+// Create an instance of a Discord client
+const client = new Client();
+
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+
+
+var moment = require('moment');
+require('moment-recur');
+
+var rInterval = moment( "2019-11-12" ).recur().every(2).weeks();
+
+
+
+
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+    // If the message is '!time'
+    if (message.content === '!time') {
+        var ts = Date.now();
+
+        var date_ob = new Date(ts);
+        var date = date_ob.getDate();
+        var month = date_ob.getMonth() + 1;
+        var year = date_ob.getFullYear();
+        rInterval.fromDate(year + "-" + month + "-" + date);
+        nextDates = rInterval.next(1, "L");
+        var nextdateobject = new Date(nextDates);
+        var nextdateDays = nextdateobject.getDate();
+        var nextdateMonth = nextdateobject.getMonth();
+        var nextdateYear = nextdateobject.getFullYear();
+        
+        message.channel.send("The next meeting will be on the :" + nextdateDays + "/" + nextdateMonth + "/" + nextdateYear);
+    }
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/ me
+client.login('NjM4NjczOTE5ODEzMDkxMzY5.XblCjg.w1ckNAEGk3Tvnb2O6c5RRcWjE5A');
+
+
+
+
+
+
+
+        
+
+
