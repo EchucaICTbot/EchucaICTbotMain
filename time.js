7eea30cf60b14357115e522438d07713c5c0fce5
Riley Brown <rileybrown3564@gmail.com>
2019-11-08 07:12:00 +1100

    make it better

--- time.js
+++ time.js
@@ -5,10 +5,7 @@ const { Client, Attachment } = require('discord.js');
 const client = new Client();
 
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
+
 
 
 var moment = require('moment');
@@ -31,13 +28,19 @@ client.on('message', message => {
 
         var date_ob = new Date(ts);
         var date = date_ob.getDate();
+        if (date.toString().length == 1) {
+            date = "0" + date;
+        }
         var month = date_ob.getMonth() + 1;
+        if (month.toString().length == 1) {
+            month = "0" + month;
+        }
         var year = date_ob.getFullYear();
         rInterval.fromDate(year + "-" + month + "-" + date);
         nextDates = rInterval.next(1, "L");
         var nextdateobject = new Date(nextDates);
         var nextdateDays = nextdateobject.getDate();
-        var nextdateMonth = nextdateobject.getMonth();
+        var nextdateMonth = nextdateobject.getMonth() + 1;
         var nextdateYear = nextdateobject.getFullYear();
         
         message.channel.send("The next meeting will be on the :" + nextdateDays + "/" + nextdateMonth + "/" + nextdateYear);
@@ -46,9 +49,6 @@ client.on('message', message => {
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/ me
 client.login('NjM4NjczOTE5ODEzMDkxMzY5.XblCjg.w1ckNAEGk3Tvnb2O6c5RRcWjE5A');
-
-
-
 
 
 
